67de242f065d8f75f11a9a51d7cf2925d068c3b1
Alberto Valido Fuentes <albertovalidofuentes2001@gmail.com>
2025-05-19 19:04:01 +0100

    Add Spring WebFlux backend base

---
+++ backend/src/main/java/com/albertovalido/chatapp/backend/ChatappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.albertovalido.chatapp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatappBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/albertovalido/chatapp/backend/ChatappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albertovalido.chatapp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
